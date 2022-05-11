2674c6e202733d4b72ced9bedde5bb23ae8d8d20
Romeo Kiekara <romeo.kiekara@gmail.com>
2022-05-11 15:38:40 +0300

    Add Composable function Layout

--- WeatherApp/app/src/main/java/fi/tuni/weatherapp/MainActivity.kt
+++ WeatherApp/app/src/main/java/fi/tuni/weatherapp/MainActivity.kt
@@ -3,12 +3,21 @@ package fi.tuni.weatherapp
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.shape.CornerSize
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.Card
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
+import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import fi.tuni.weatherapp.ui.theme.WeatherAppTheme
 
 class MainActivity : ComponentActivity() {
@@ -31,7 +40,19 @@ class MainActivity : ComponentActivity() {
 
 @Composable
 fun Layout() {
-
+    Card(
+        modifier = Modifier.padding(12.dp),
+        shape = RoundedCornerShape(corner = CornerSize(16.dp)),
+        elevation = 4.dp
+    ) {
+        Column(
+            modifier = Modifier.padding(16.dp),
+            verticalArrangement = Arrangement.Top,
+            horizontalAlignment = Alignment.CenterHorizontally
+        ) {
+            Text(text = "some information")
+        }
+    }
 }
 
 @Preview(showBackground = true)
