2749afd92b433ac293a08ec1143b68c317303211
Romeo Kiekara <romeo.kiekara@gmail.com>
2022-05-16 02:26:31 +0300

    Add function fetchData

--- WeatherApp/app/src/main/java/fi/tuni/weatherapp/Util.kt
+++ WeatherApp/app/src/main/java/fi/tuni/weatherapp/Util.kt
@@ -1,5 +1,8 @@
 package fi.tuni.weatherapp
 
+import okhttp3.OkHttpClient
+import okhttp3.Request
+import java.io.IOException
 import java.net.URL
 
 fun constructWeatherOrForecastUrl(
@@ -27,4 +30,20 @@ fun constructWeatherAndForecastUrls(
         constructWeatherOrForecastUrl(city = city, lat = lat, lon = lon),
         constructWeatherOrForecastUrl(searchKey = "forecast", city = city, lat = lat, lon = lon)
     )
+}
+
+fun fetchData(url: URL): String {
+    val client = OkHttpClient()
+
+    val request = Request.Builder()
+        .url(url = url)
+        .build()
+
+    client.newCall(request = request).execute().use {
+        if (!it.isSuccessful) throw IOException("Unexpected code $it")
+
+        for ((name, value) in it.headers) println("$name: $value")
+
+        return it.body.string()
+    }
 }
