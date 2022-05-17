6782dd199a11119a754f42f18e44ce1998e98076
Romeo Kiekara <romeo.kiekara@gmail.com>
2022-05-17 10:23:16 +0300

    Add unknown properties ignoration to data classes

--- WeatherApp/app/src/main/java/fi/tuni/weatherapp/data/ForecastData.kt
+++ WeatherApp/app/src/main/java/fi/tuni/weatherapp/data/ForecastData.kt
@@ -1,10 +1,14 @@
 package fi.tuni.weatherapp.data
 
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties
+
+@JsonIgnoreProperties(ignoreUnknown = true)
 data class ForecastJsonObject(
     var list: MutableList<WeatherItem>? = null,
     var city: City? = null
 )
 
+@JsonIgnoreProperties(ignoreUnknown = true)
 data class WeatherItem(
     var dt: Long = 0,
     var main: Temperature? = null,
@@ -12,6 +16,7 @@ data class WeatherItem(
     var wind: Wind? = null
 )
 
+@JsonIgnoreProperties(ignoreUnknown = true)
 data class City(
     var name: String? = null,
     var country: String? = null,
--- WeatherApp/app/src/main/java/fi/tuni/weatherapp/data/WeatherData.kt
+++ WeatherApp/app/src/main/java/fi/tuni/weatherapp/data/WeatherData.kt
@@ -1,5 +1,8 @@
 package fi.tuni.weatherapp.data
 
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties
+
+@JsonIgnoreProperties(ignoreUnknown = true)
 data class WeatherJsonObject(
     var weather: MutableList<Weather>? = null,
     var main: Temperature? = null,
@@ -7,16 +10,19 @@ data class WeatherJsonObject(
     var name: String? = null
 )
 
+@JsonIgnoreProperties(ignoreUnknown = true)
 data class Weather(
     var main: String? = null,
     var description: String? = null
 )
 
+@JsonIgnoreProperties(ignoreUnknown = true)
 data class Temperature(
     var temp: Double = 0.0,
     var feels_like: Double = 0.0
 )
 
+@JsonIgnoreProperties(ignoreUnknown = true)
 data class Wind(
     var speed: Double = 0.0
 )
