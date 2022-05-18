3ee4d34fc7a0f7ecdbe329e3a99716820d0d6291
Romeo Kiekara <romeo.kiekara@gmail.com>
2022-05-18 03:43:34 +0300

    Add data class DayTime to WeatherJsonObject

--- WeatherApp/app/src/main/java/fi/tuni/weatherapp/data/WeatherData.kt
+++ WeatherApp/app/src/main/java/fi/tuni/weatherapp/data/WeatherData.kt
@@ -7,6 +7,7 @@ data class WeatherJsonObject(
     var weather: MutableList<Weather>? = null,
     var main: Temperature? = null,
     var wind: Wind? = null,
+    var sys: DayTime? = null,
     var name: String? = null
 )
 
@@ -19,10 +20,17 @@ data class Weather(
 @JsonIgnoreProperties(ignoreUnknown = true)
 data class Temperature(
     var temp: Double = 0.0,
-    var feels_like: Double = 0.0
+    var feels_like: Double = 0.0,
+    var humidity: Int = 0
 )
 
 @JsonIgnoreProperties(ignoreUnknown = true)
 data class Wind(
     var speed: Double = 0.0
+)
+
+@JsonIgnoreProperties(ignoreUnknown = true)
+data class DayTime(
+    var sunrise: Long = 0,
+    var sunset: Long = 0
 )
